533fe75dfa6712d5af499235624f7d11bba699bd
LReinacher <lion.reinacher@gmail.com>
2020-12-04 16:17:38 +0100

    exercise 3

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import numpy as np
 import sys
 
 
-class SlidingGame:
+class SlidingGameMatch:
     def __init__(self):
-        self._board = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, np.NaN, 12], [13, 14, 11, 15]])
+        self._board = np.array([[1, 2, 3], [5, 6, 7], [9, 10, np.NaN], [13, 14, 11]])
         self._valid_numbers = self._board[~np.isnan(self._board)]
+        self._move_counter = 0
 
     def is_number_valid(self, number: int):
         return number in self._valid_numbers
@@ -45,25 +46,39 @@ class SlidingGame:
             return True
         return False
 
-    def play(self):
-        print("--- SLIDING PUZZLE ---")
+    def play_match(self):
         self._print_board()
-        print("Please enter the number of the piece you want to move")
         while not self._is_board_ordered():
             piece = input(">> ")
             try:
                 piece_number = int(piece)
-                if not game.is_number_valid(piece_number):
+                if not self.is_number_valid(piece_number):
                     raise TypeError
                 if not self._move_piece_by_number(piece_number):
                     print("This move is not allowed!")
                     continue
+                self._move_counter += 1
                 self._print_board()
             except TypeError:
                 print("Invalid number, please try again")
 
-        print("GAME WON!")
-        sys.exit(0)
+        return self._move_counter
+        
+class SlidingGame:
+    def __init__(self):
+        self._matches_moves_counter = np.array([np.NaN, np.NaN, np.NaN])
+        
+    def play(self):
+        print("--- SLIDING PUZZLE ---")
+        for match_number in range(0, self._matches_moves_counter.size):
+            print("++++ MATCH {} ++++".format(match_number + 1))
+            print("Please enter the number of the piece you want to move")
+            match = SlidingGameMatch()
+            self._matches_moves_counter[match_number] = match.play_match()
+            print("==== MATCH FINISHED ====".format(match_number + 1))
+        print("In your best match, you needed {} moves to solve the puzzle!".format(int(self._matches_moves_counter.min())))
+
+            
 
 if __name__ == "__main__":
     game = SlidingGame()
